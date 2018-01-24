1ae62a204c3aaebdfb038ac18b9b719afa8bdf7d
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-01-24 17:00:18 +0200

    Edited 14th Problem

--- Expressions-ConditionalStatements-Loops/biggestOf3Numbers.js
+++ Expressions-ConditionalStatements-Loops/biggestOf3Numbers.js
@@ -1,15 +1,5 @@
 function calculateBiggestNumber([n1, n2, n3]) {
-    let biggestNum = n1
-
-    if (biggestNum <= n2) {
-        biggestNum = n2
-    }
-
-    if (biggestNum <= n3) {
-        biggestNum = n3
-    }
-
-    return biggestNum
+    return Math.max(n1, n2, n3)
 }
 
 console.log(calculateBiggestNumber([5, 5, 5]))
