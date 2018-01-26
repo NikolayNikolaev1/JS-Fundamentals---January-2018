63d11a47fc0a2cdc31a9016d2b08b09ab9229684
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-01-26 21:44:16 +0200

    Edited Problem 1

--- 3. FunctionsAndArrowFunctions/triangleOfStars.js
+++ 3. FunctionsAndArrowFunctions/triangleOfStars.js
@@ -1,10 +1,14 @@
 function triangleOfStars(n) {
     for (let i = 1; i <= n; i++) {
-        console.log("*".repeat(i))
+        print(i)
     }
 
     for (let i = n - 1; i >= 1; i--) {
-        console.log("*".repeat(i))
+        print(i)
+    }
+    
+    function print(n) {
+        console.log("*".repeat(n))
     }
 }
 
